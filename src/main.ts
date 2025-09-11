48196fe9ea0d199f118dac212373ae6c1d1eeb4b
Marc <14913675+MarcStdt@users.noreply.github.com>
2025-09-11 23:08:16 +0200

    chore: onchange event

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { createRoutingStrategy } from "./routing-strategy";
 
 export type RerenderFn = () => void;
 
-const allowedProps = ["onclick", "onkeyup", "type"] as Array<keyof HTMLElement>;
+const allowedProps = ["onclick", "onkeyup", "onchange", "type"] as Array<keyof HTMLElement>;
 
 function updateEvents(fromEl: any, toEl: any) {
   var i, eventPropName;
