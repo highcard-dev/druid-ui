995b818dbf3239f98afba508e95b18196295940c
Marc Schottstädt <marc.schottstaedt@gmail.com>
2025-11-24 10:16:33 +0100

    chore: world name in cli

--- src/build/cmd/index.ts
+++ src/build/cmd/index.ts
@@ -5,6 +5,7 @@ console.log("Druid UI Build Tool");
 let args = process.argv;
 
 let duBuildRaw = false;
+let worldName = "druid-ui";
 const witFiles: string[] = [];
 
 args = args.filter((arg) => {
@@ -16,13 +17,19 @@ args = args.filter((arg) => {
     witFiles.push(arg.slice(6));
     return false;
   }
+  if (arg.startsWith("--world-name=")) {
+    worldName = arg.slice(13);
+    return false;
+  }
   return true;
 });
 
 const entryFile = args[2];
 
 if (!entryFile) {
-  console.error("Usage: ./cli <entry-file> [out-folder]");
+  console.error(
+    "Usage: ./cli <entry-file> [out-folder] [--world-name=<name>] [--wit=<file>] [--raw]"
+  );
   process.exit(1);
 }
 
@@ -32,7 +39,7 @@ if (duBuildRaw) {
   await buildRaw(entryFile, outfolder);
 } else {
   await buildWasm(entryFile, outfolder, {
-    worldName: "druid-ui",
+    worldName,
     files: witFiles,
   });
 }
