a9ad48ace4a741f5faee48a5dfc35daa82c51e03
Marc Schottstädt <marc.schottstaedt@gmail.com>
2025-11-28 21:24:14 +0100

    chore: load-file-from-deployment

--- src/plattform/index.ts
+++ src/plattform/index.ts
@@ -1,6 +1,16 @@
 /// <reference types="./types/druid-plattform.d.ts" />
 /// <reference types="../component/jsx.d.ts" />
-import { request as requestRaw } from "druid:ui/plattform";
+import {
+  request as requestRaw,
+  loadFileFromDeployment as loadFileFromDeploymentRaw,
+  saveFileToDeployment as saveFileToDeploymentRaw,
+} from "druid:ui/plattform";
 import { rawAsyncToPromise } from "../../src/component/utils";
 
 export const request = rawAsyncToPromise(requestRaw);
+
+export const loadFileFromDeployment = rawAsyncToPromise(
+  loadFileFromDeploymentRaw
+);
+
+export const saveFileToDeployment = rawAsyncToPromise(saveFileToDeploymentRaw);
