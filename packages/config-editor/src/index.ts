230dace7a32c543cee5206fc69d920aebef39de8
HelpMe <marvin@highcard.dev>
2026-08-13 20:32:15 +0200

    feat: add configuration editor manifest contract

---
+++ packages/config-editor/src/index.ts
@@ -0,0 +1,12 @@
+export { fieldsForVersion, validateManifest } from "./manifest.js";
+export type {
+  ConfigEditorManifest,
+  ConfigFormat,
+  ConfigValue,
+  FieldSchema,
+  FieldType,
+  FileSchema,
+  SectionSchema,
+  ServerSchema,
+  ValidationIssue,
+} from "./model.js";
